f1a727495f24edb53baeb13eed1942c0d17f5cd4
Amar Khedkar <amarkhedkar@outlook.com>
2025-04-10 02:07:26 +0530

    FIRST COMMIT
    Encode URL of website to get encrypted and small domain name that will save the costing of DOMAIN buy/selll and User will get the Same URL on searching their site on google

---
+++ src/main/java/com/smarty/EncodeURL/EncodeUrlApplication.java
@@ -0,0 +1,13 @@
+package com.smarty.EncodeURL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncodeUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncodeUrlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smarty/EncodeURL/EncodeUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarty.EncodeURL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncodeUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
